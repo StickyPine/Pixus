38543d2455e63d4d2a9b2ccdb3d483e1e21a03e8
bhouart <56756819+bhouart@users.noreply.github.com>
2023-10-03 18:06:48 +0200

    Added Windows window manager

--- src/pixus.py
+++ src/pixus.py
@@ -1,4 +1,4 @@
-from windows import WindowManagerX11
+from windows import WindowManagerX11, WindowManagerWindows
 from bot_worker import BotWorker
 from view import PixusView
 from PyQt6.QtWidgets import QApplication
@@ -8,8 +8,15 @@ import sys
 def main():
     app = QApplication(sys.argv)
     model_path = "../yolov8_models/best_chataignier_only.pt"
+    
+    # X11
     wm11 = WindowManagerX11("dofus.exe")
     bot_worker = BotWorker(wm11, model_path)
+    
+    # Windows
+    # wmwindows = WindowManagerWindows("Dofus")
+    # bot_worker = BotWorker(wmwindows, model_path)
+    
     pixus = PixusView(bot_worker)
     pixus.show()
     sys.exit(app.exec())
--- src/windows.py
+++ src/windows.py
@@ -2,6 +2,8 @@ from Xlib import X, display as Xdisplay
 from abc import ABC, abstractmethod
 import numpy as np
 import cv2
+import pygetwindow as gw
+import win32gui, win32con, win32ui
 from typing import *
 
 
@@ -76,3 +78,64 @@ class WindowManagerX11(WindowManagerAbstract):
         image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
         return image
 
+@final
+class WindowManagerWindows(WindowManagerAbstract):
+
+    def __init__(self, window_title: str):
+        self.__window_title = window_title
+        self.__window = None
+        self.hwnd = None
+        self.grab_window()
+        # to crop the window border and titlebar off the image
+        self.cropped_x = 8
+        self.cropped_y = 30
+
+    def grab_window(self):
+        window_names = gw.getAllTitles()
+        matching_names = [name for name in window_names if self.__window_title in name]
+        if (len(matching_names) == 0):
+            raise ValueError(f"Window with title '{self.__window_title}' not found.")
+        self.hwnd = win32gui.FindWindow(None, matching_names[0])
+        self.__window = gw.getWindowsWithTitle(matching_names[0])[0]        
+
+    def translate_position(self, x: int, y: int) -> Tuple[int, int]:
+        left, top, _, _ = self.__window.left, self.__window.top, self.__window.width, self.__window.height
+        return left + x + self.cropped_x, top + y + self.cropped_y
+
+    def resize_window(self, width: int, height: int) -> None:
+        self.__window.resizeTo(width, height)
+
+    def get_window_array(self) -> np.ndarray:
+        
+        # get the window size
+        window_rect = win32gui.GetWindowRect(self.hwnd)
+        self.w = window_rect[2] - window_rect[0]
+        self.h = window_rect[3] - window_rect[1]
+        # account for the window border and titlebar and cut them off
+        self.w = self.w - (self.cropped_x * 2)
+        self.h = self.h - self.cropped_y - self.cropped_x
+        
+        # get the window image data
+        wDC = win32gui.GetWindowDC(self.hwnd)
+        dcObj = win32ui.CreateDCFromHandle(wDC)
+        cDC = dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, self.w, self.h)
+        cDC.SelectObject(dataBitMap)
+        cDC.BitBlt((0, 0), (self.w, self.h), dcObj, (self.cropped_x, self.cropped_y), win32con.SRCCOPY)
+
+        # convert the raw data into a NumPy array
+        signedIntsArray = dataBitMap.GetBitmapBits(True)
+        img = np.frombuffer(signedIntsArray, dtype='uint8')
+        img.shape = (self.h, self.w, 4)
+
+        # free resources
+        dcObj.DeleteDC()
+        cDC.DeleteDC()
+        win32gui.ReleaseDC(self.hwnd, wDC)
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+        
+        img = img[...,:3]
+        img = np.ascontiguousarray(img)
+        return img
+    
